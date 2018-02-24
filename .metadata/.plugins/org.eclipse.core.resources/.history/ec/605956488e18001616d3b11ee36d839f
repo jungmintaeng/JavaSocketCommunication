f55e9beb7e6f6be13b7547ef76a109b52136bef6
jungmintaeng <jungmin4025@gmail.com>
2018-02-24 13:31:26 +0900

    java socket communication

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/ec/605956488e18001616d3b11ee36d839f
@@ -0,0 +1,8 @@
+
+public class ClienMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Client cli = new Client("180.182.188.140");
+	}
+}
---
+++ Server/src/Servermain.java
@@ -0,0 +1,11 @@
+import java.sql.SQLException;
+
+public class Servermain {
+
+	public static void main(String[] args) throws SQLException {
+		// TODO Auto-generated method stub
+		Server serverstart = new Server();
+		serverstart.ServerStart();
+	}
+
+}
